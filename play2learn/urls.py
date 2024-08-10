7dcfaf5f8bec4a7c38b28a9e007dc21c5cf69bf3
Kuro0731 <woodcoty99@gmail.com>
2024-08-10 03:41:51 -0500

    fixed users app

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
 
-    # User Management
+     # User Management
+    path("account/", include("users.urls")),
     path("account/", include("allauth.urls")),
 
     # Local Apps
